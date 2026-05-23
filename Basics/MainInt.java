9370a33ad83ac8be51df912d8b8bd41ded3ae41c
Ritika <ritika.learn88@gmail.com>
2026-05-23 21:30:39 +0530

    Primitive Datatypes

--- Basics/MainInt.java
+++
@@ -1,7 +0,0 @@
-public class MainInt {
-    public static void main(String[] args){
-        int temperature=45;
-        System.out.println("Temperature");
-    }
-    
-}
---
+++ Basics/Primitives/MainInt.java
@@ -0,0 +1,127 @@
+public class MainInt{
+
+public static void main(String[] args){
+
+    int temp=45;    //current temperature is 45
+    System.out.println(temp);   //printing the current temperature
+
+
+
+    //int aadharnumber=345678678456; i cannot store larger num into small integer , 12 digit num i cannot store
+
+
+    //int contactnumber= 9561965698; i cannot store a 10 digit num also
+
+
+    int age=34;
+    System.out.println(age);
+
+
+    //int height=6.6;  int datatype cannot store decimal number
+
+    int weight=30;
+     System.out.println(weight);
+
+
+     int rank=1;
+     System.out.println(rank);
+
+     
+     int rollno=11;
+     System.out.println(rollno);
+
+
+     int id=8274;
+     System.out.println(id);
+
+
+     //int panCardNo=4566786546;  we can't store pan card num , it stores 10 digits
+
+
+     int luckyNumber=88;
+     System.out.println(luckyNumber);
+
+
+     int houseNumber=835;
+     System.out.println(houseNumber);
+
+
+     //int time=4:56;  invalid AssignmentOperators 
+
+
+    int date=15;
+    System.out.println(date);
+
+
+    int bankAccNumber=787345567;
+    System.out.println(bankAccNumber);
+
+    
+    int pageNumber=49;
+    System.out.println(pageNumber);
+
+
+    int recieptNumber=1984;
+    System.out.println(recieptNumber);
+
+
+    int specsNumber=-2;
+    System.out.println(specsNumber);
+
+
+    int billNumber=3330;
+    System.out.println(billNumber);
+
+
+    int roomNumber=07;
+    System.out.println(roomNumber);
+
+
+    int password=6032007;
+    System.out.println(password);
+
+    int fileNumber=1;
+    System.out.println(fileNumber);
+
+
+    int score=132;
+    System.out.println(score);
+
+
+    int marks=90;
+    System.out.println(marks);
+
+
+    int year=2007;
+    System.out.println(year);
+
+
+    int memoryStorage=64;
+    System.out.println(memoryStorage);
+
+
+    int numberOfDays=31;
+    System.out.println(numberOfDays);
+
+
+    int numberOfEmails=2000;
+    System.out.println(numberOfEmails);
+
+
+    int unreadMessages=48;
+    System.out.println(unreadMessages);
+
+
+    int inbox=76;
+    System.out.println(inbox);
+
+
+    int numberOfsentItems=23;
+    System.out.println(numberOfsentItems);
+
+
+    int numberOfDeletedItems=65;
+    System.out.println(numberOfDeletedItems);
+
+}
+}
