ebaefda801ac2be0bee54db38df27c0f7302f7cb
Ritika <ritika.learn88@gmail.com>
2026-05-25 18:10:31 +0530

    Stage 3 : MainByte.java

---
+++ Basics/Primitives/MainByte.java
@@ -0,0 +1,138 @@
+public class MainByte {
+public static void main(String[] args){
+   byte students=90;  //total students
+   System.out.println("Total no.of students: "+ students);  //showing students
+
+
+   byte hours=12;  //study hours
+   System.out.println("I've studied for "+ hours +"hours");  //printing hours
+
+   byte games=5; //games played
+   System.out.println("Number of games: "+ games);  //showing no.of games
+
+
+   byte score=99;  //match score
+   System.out.println("how much they scored: "+ score);  //printing the score
+
+
+   byte apples=20;  //apples count
+   System.out.println("how many apples are there in basket: "+ apples);  //displaying apples
+
+
+   byte banana=12;  //counting banana's
+   System.out.println("how many banana's are there in one dozen: "+ banana); //printing bananas
+
+
+   byte salary=100;  //salary amount
+   System.out.println("How much salary do you got: "+ salary);   //showing salary
+
+
+   byte level=5;   //game level
+   System.out.println("how many levels you've crossed till now: "+ level);   //showing the levels
+
+
+   byte days=7;  //days in week
+   System.out.println("how many days are there in one week: "+ days);   //printing weeks
+
+
+   byte month=3;   //birth month
+   System.out.println("My birth month is: "+ month);   //showing month
+
+
+   byte rank=1;   //class rank
+   System.out.println("I got "+ rank + "in my class");   //printing rank
+
+
+   byte speed= 80;   //car speed
+   System.out.println("The car speed is" + speed +  "km/h");   //displaying the speed
+
+
+   byte weight=55;    //body weight
+   System.out.println("My dad's weight is" + weight );   //showing weight
+
+
+   byte price=99;   //biscuits price
+   System.out.println("The price of biscuits was" + price);   //displaying the price
+
+
+   byte distance=45;   //travel distance
+   System.out.println("The distance is" + distance);   //the total distance
+
+
+   byte battery=85;   //mobile battery
+   System.out.println("My phone battery is" + battery);  //displaying the battery
+
+
+   byte rooms=5;  //number of rooms
+   System.out.println("There are" + rooms + "in this house");    //printing rooms
+
+
+
+   byte tickets=2;   //train tickets
+   System.out.println("I booked " + tickets + "tickets");  //number of tickets
+
+
+   byte cousins=5;   //cousins count
+   System.out.println("I've" + cousins + "cousins");   //displaying cousins
+
+   byte goals=3;    //football goals 
+   System.out.println("The player scored" + goals +"goals");   //printing the goals
+
+
+   byte pets=4;   //pets count 
+   System.out.println("I've total " + pets +"pets");   //showing pets
+
+   
+   byte shoes=9;   //pairs of shoes
+   System.out.println("I got" + shoes + "pairs of shoes");   //printing total no.of pairs of shoes
+
+
+   byte calls=6;   // number of calls
+   System.out.println("I recieved" + calls + "from my dad");   //printing the total calls
+
+
+   byte coins=50;  //coins collected
+   System.out.println("I've total " + coins + "coins in my pocket");   //showing the coins
+
+
+   byte stars=5;  //counting stars
+   System.out.println("There are only" + stars + "in the sky");   //displaying the stars
+
+
+   byte pages=70;   //number of pages incomplete
+   System.out.println("I only left " + pages + "pages more to complete");   //printing pages
+
+
+   byte pens=12;   //pens I've with me
+   System.out.println("Today I bought" + pens + "pens");  //number of pens i bought
+
+
+   byte videos=6;  //total number of videos 
+   System.out.println("I shoot total" + videos + "videos");   //dispalying the videos
+
+
+   byte chocolates=11;   //number of chocolates i ate today
+   System.out.println("Today I ate total " + chocolates + "chocolates");   //showing chocolates
+
+
+
+   byte subjects=7;   //total number of subjects 
+   System.out.println("Today I'm done with " + subjects + "subjects");  //printing the number of subjects i've done 
+
+
+   byte waterBottles=13;   //number of bottles
+   System.out.println("My muom bought total" + waterBottles + "Water bottles");    //showing the number of bottles
+
+
+
+   byte laptops=3;   //number of laptops 
+   System.out.println("My uncle have total" + laptops + "laptops with him");   //showing the number of laptops my uncle have
+
+
+
+   byte holidays=10;    //number of holidays i got
+   System.out.println("I got total" + holidays + "days of holidays");    //displaying the total no.of holidays
+
+
+}
+}
