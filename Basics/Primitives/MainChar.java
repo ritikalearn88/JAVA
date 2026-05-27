886cd595a7af3236dc28e4027a58c11c0f8d2d84
Ritika <ritika.learn88@gmail.com>
2026-05-27 19:46:25 +0530

    Stage 5 : MainChar.java

---
+++ Basics/Primitives/MainChar.java
@@ -0,0 +1,241 @@
+public class MainChar {
+
+    public static void main(String[] args) {
+
+        char grade = 'A'; //student grade
+        System.out.println("Student grade: " + grade); //showing grade
+
+
+
+        char section = 'B'; //class section
+        System.out.println("Class section: " + section); //displaying section
+
+
+
+        char gender = 'F'; //gender initial
+        System.out.println("Gender initial: " + gender); //printing gender
+
+
+
+        char bloodGroup = 'O'; //blood group character
+        System.out.println("Blood group starts with: " + bloodGroup); //showing blood group
+
+
+
+        char firstLetter = 'R'; //name first letter
+        System.out.println("First letter of name: " + firstLetter); //displaying first letter
+
+
+
+        char symbol = '@'; //special symbol
+        System.out.println("Special symbol used: " + symbol); //printing symbol
+
+
+
+        char currency = '$'; //currency symbol
+        System.out.println("Currency symbol: " + currency); //showing currency
+
+
+
+        char operator = '+'; //math operator
+        System.out.println("Math operator: " + operator); //displaying operator
+
+
+
+        char answer = 'Y'; //yes or no answer
+        System.out.println("Answer selected: " + answer); //printing answer
+
+
+
+        char size = 'M'; //shirt size
+        System.out.println("Shirt size: " + size); //showing size
+
+
+
+        char direction = 'N'; //direction symbol
+        System.out.println("Direction selected: " + direction); //displaying direction
+
+
+
+        char option = 'C'; //mcq option
+        System.out.println("Chosen option: " + option); //printing option
+
+
+
+        char vowel = 'E'; //vowel character
+        System.out.println("Selected vowel: " + vowel); //showing vowel
+
+
+
+        char code = 'X'; //secret code letter
+        System.out.println("Security code letter: " + code); //displaying code
+
+
+
+        char subject = 'J'; //subject initial
+        System.out.println("Subject initial: " + subject); //printing subject
+
+
+
+        char trainCoach = 'D'; //train coach
+        System.out.println("Train coach: " + trainCoach); //showing coach
+
+
+
+        char gameLevel = 'H'; //game difficulty
+        System.out.println("Game level selected: " + gameLevel); //displaying level
+
+
+
+        char planet = 'M'; //planet initial
+        System.out.println("Planet initial: " + planet); //printing planet
+
+
+        
+        char category = 'P'; //product category
+        System.out.println("Product category: " + category); //showing category
+
+
+        char light = 'G'; //traffic signal
+        System.out.println("Traffic signal light: " + light); //displaying signal
+
+
+
+        char rank = 'S'; //special rank
+        System.out.println("Rank achieved: " + rank); //printing rank
+
+
+
+        char ticketType = 'V'; //vip ticket
+        System.out.println("Ticket type: " + ticketType); //showing ticket type
+
+
+
+        char laptopGrade = 'A'; //laptop quality
+        System.out.println("Laptop quality grade: " + laptopGrade); //displaying quality
+
+        
+        
+        char busRoute = 'K'; //bus route
+        System.out.println("Bus route code: " + busRoute); //printing route
+
+
+
+        char examCode = 'Z'; //exam code
+        System.out.println("Exam code letter: " + examCode); //showing exam code
+
+
+
+        char buildingBlock = 'T'; //building block
+        System.out.println("Building block: " + buildingBlock); //displaying block
+
+
+
+        char foodGrade = 'B'; //food rating
+        System.out.println("Food quality grade: " + foodGrade); //printing food grade
+
+
+
+        char team = 'R'; //team name initial
+        System.out.println("Team initial: " + team); //showing team
+
+
+
+        char language = 'J'; //language initial
+        System.out.println("Programming language initial: " + language); //displaying language
+
+
+
+        char batteryStatus = 'L'; //battery condition
+        System.out.println("Battery status level: " + batteryStatus); //printing battery level
+
+        
+        
+        char wifiSignal = 'F'; //wifi signal strength
+        System.out.println("WiFi signal strength: " + wifiSignal); //showing signal
+
+
+
+        char movieRating = 'U'; //movie rating
+        System.out.println("Movie rating category: " + movieRating); //displaying movie rating
+
+
+
+        char shirtCode = 'Q'; //shirt code
+        System.out.println("Shirt code letter: " + shirtCode); //printing shirt code
+
+
+
+        char appVersion = 'V'; //app version
+        System.out.println("Application version letter: " + appVersion); //showing version
+
+
+
+
+        char passwordHint = 'P'; //password hint
+        System.out.println("Password hint starts with: " + passwordHint); //displaying password hint
+
+
+
+
+        char keyboardKey = '#'; //keyboard symbol
+        System.out.println("Keyboard key selected: " + keyboardKey); //printing key
+
+
+         //char name = 'AB';
+        //This will give error because char cannot store multiple characters
+
+
+        
+        char temperatureGrade = 'C'; //temperature unit
+        System.out.println("Temperature unit: " + temperatureGrade); //showing temperature unit
+
+
+
+        char shape = 'O'; //shape initial
+        System.out.println("Shape initial selected: " + shape); //displaying shape
+
+
+
+        char mobileSeries = 'S'; //mobile series
+        System.out.println("Mobile series code: " + mobileSeries); //printing series
+
+
+
+        char hotelType = 'L'; //luxury hotel
+        System.out.println("Hotel category: " + hotelType); //showing hotel type
+
+
+
+        char parkingZone = 'P'; //parking zone
+        System.out.println("Parking zone letter: " + parkingZone); //displaying parking zone
+
+
+
+        char roadType = 'H'; //highway type
+        System.out.println("Road type code: " + roadType); //printing road type
+
+
+
+        char examResult = 'P'; //pass result
+        System.out.println("Exam result status: " + examResult); //showing result
+
+
+
+        char musicNote = 'D'; //music note
+        System.out.println("Music note selected: " + musicNote); //displaying note
+
+
+
+        char medalType = 'G'; //gold medal
+        System.out.println("Medal type won: " + medalType); //printing medal
+
+
+
+        char officeBlock = 'A'; //office block
+        System.out.println("Office block assigned: " + officeBlock); //showing office block
+
+
+        
+    }
+}
